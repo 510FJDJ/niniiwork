f19bd512bfc83394a06ece33c42b21f0241474c1
ninii <wuy0951@gmail.com>
2024-09-27 17:55:28 +0800

    new project

---
+++ main.js
@@ -0,0 +1,7 @@
+// import "./stylesheet/all.css";
+// console.log("main.js 文件已成功載入！");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const header = document.querySelector("header");
+//     header.innerHTML = "<h1>你好！歡迎來到我的作品集</h1>";
+// });
